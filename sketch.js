b94b71cffccc8989aa9433206b39c1b8a9bc7248
Warren Longmire <warrenlongmire@gmail.com>
2019-07-19 09:09:13 -0400

    initial commit

---
+++ sketch.js
@@ -0,0 +1,33 @@
+/*
+Pseudo code:
+
+Main goals:
+1. Create a ship at the bottom of the screen.
+
+2. Allow that ship to move to the left or right when the user clicks on the approprate keyboard button.
+
+3. When the user clicks on the space bar, allow for a projectile to shoot from the ship upward.
+
+4. Create an enemy that can be destroyed when the projectile hits it.
+5. Display a win screen if enemy has been destroyed.
+
+Extra credit:
+1. Allow the enemy to move in a pattern similar to traditional space invaders.
+2. Allow the enemy to be able to hurt the player when touched (one hit kills are ok)
+3. Create a way to reset the board
+4. Track the users score
+  - Number of hits
+  - Number of ships remaining
+5. Randomize enemy speed.
+
+*/
+
+function preload() {
+
+}
+
+function setup() {
+}
+
+function draw() {
+}
