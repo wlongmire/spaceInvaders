6de9808053f4a7e3fd83b8d7273804c89f70fdec
Warren Longmire <warrenlongmire@gmail.com>
2019-07-19 09:11:00 -0400

    adding to pseudocode

--- sketch.js
+++ sketch.js
@@ -2,14 +2,17 @@
 Pseudo code:
 
 Main goals:
-1. Create a ship at the bottom of the screen.
+1. Create a canvas and a black background.
 
-2. Allow that ship to move to the left or right when the user clicks on the approprate keyboard button.
+2. Create a ship at the bottom of the screen.
 
-3. When the user clicks on the space bar, allow for a projectile to shoot from the ship upward.
+3. Allow that ship to move to the left or right when the user clicks on the approprate keyboard button.
 
-4. Create an enemy that can be destroyed when the projectile hits it.
-5. Display a win screen if enemy has been destroyed.
+4. When the user clicks on the space bar, allow for a projectile to shoot from the ship upward.
+
+5. Create an enemy that can be destroyed when the projectile hits it.
+
+6. Display a win screen if enemy has been destroyed.
 
 Extra credit:
 1. Allow the enemy to move in a pattern similar to traditional space invaders.
