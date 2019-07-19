8885c4102283cd08940164ce948500a8b7e31fb7
Warren Longmire <warrenlongmire@gmail.com>
2019-07-19 15:29:37 -0400

    made a game

--- sketch.js
+++ sketch.js
@@ -25,12 +25,58 @@ Extra credit:
 
 */
 
-function preload() {
+var shipX;
+var shipY;
 
+var bulletX, bulletY;
+
+var shipImage;
+var spaceBkg;
+
+var isShooting;
+
+function preload() {
+  shipImage = loadImage('images/spaceship.png');
+  spaceBkg = loadImage('images/background.jpeg')
 }
 
 function setup() {
+  createCanvas(500, 500);
+  shipX = width / 2 - 25;
+  shipY = height - 100;
+
+  bulletX = shipX;
+  bulletY = shipY;
+
+  isShooting = false;
 }
 
+var dir = 1;
+
 function draw() {
+  background("black");
+  // image(spaceBkg, 0, 0);
+
+  //bullet
+  fill('yellow');
+  circle(bulletX, bulletY, 20);
+
+  if (keyIsDown(ENTER)) {
+    isShooting = true;
+    bulletY = shipY;
+    bulletX = shipX;
+  }
+
+  if (isShooting) {
+    bulletY -= 3;
+  }
+
+
+  if (keyIsDown(RIGHT_ARROW)) {
+    shipX += 1;
+  } else if (keyIsDown(LEFT_ARROW)) {
+    shipX -= 1;
+  }
+
+  image(shipImage, shipX, shipY);
 }
